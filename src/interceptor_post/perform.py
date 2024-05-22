435049725dee85838eebf0d8685c38c1fa25b082
Dan Tamam <dant@biot-med.com>
2024-05-22 13:07:43 +0300

    Fixed response body returned to the service in post and post entity interception modules

--- src/interceptor_post/perform.py
+++ src/interceptor_post/perform.py
@@ -28,4 +28,4 @@ def perform(data, token, traceparent, metadata):
 
     # ----------------------------------------------------------------------------------
 
-    return changed_data["body"]["response"]
+    return {"response": changed_data["body"]["response"]}
